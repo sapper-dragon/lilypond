f246a284ade0a4c351aa78d7af26f216cc1aaa05
Robert Hall <thebookofrobert@gmail.com>
2019-06-09 17:43:57 -0600

    Remove command

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const exec = require('util').promisify(require('child_process').exec)
-import { yellow } from 'ansi-colors'
+import { yellowBright, red } from 'ansi-colors'
 import { refresh } from '../trimmings/tools'
 
 export const change = async({ config, filepath }) => {
 	try {
-		const output = filepath.replace(/\.ly$/, '')
-		const command = `lilypond --output=./${config.lilypond.input}/${output} ./${config.lilypond.input}/${filepath}`
+		const { input, filter, outputStatic } = config.lilypond
+		const output = filepath.replace(filter, '')
+		const command = `lilypond --output=./${outputStatic}/${output} ./${input}/${filepath}`
 		const { stdout } = await exec(command)
 		if (stdout) { console.log(stdout) }
 		await refresh()
-		console.log('~>', yellow('Finished converting LilyPond file:'), filepath)
-		console.log()
+		console.log('~>', yellowBright('Finished converting LilyPond file:'), filepath)
 	} catch (error) {
-		console.error(`error: ${error}`)
+		console.error(red('Error:'))
+		console.error(error)
 	}
+	console.log()
+}
+
+export const remove = async({ config, filepath }) => {
+	try {
+		const { filter, outputStatic } = config.lilypond
+		const path = `${outputStatic}/${filepath.replace(filter, '.pdf')}`
+		const { stdout } = await exec(`rm -f ${path}`)
+		if (stdout) { console.log(stdout) }
+		console.log('~>', yellowBright('Removing pdf file:'), path)
+	} catch (error) {
+		console.error(red('Error:'))
+		console.error(error)
+	}
+	console.log()
 }
